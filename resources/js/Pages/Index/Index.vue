cb58589ba2eb452c4ca59d9d322e73ee6486376a
TimothyPandaRed <timothy@chewedleashphotography.com>
2024-07-13 16:43:58 +0100

    Initial Commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container mx-auto grid place-content-center w-dvw h-dvh">
+        <p class="text-4xl font-bold text-blue-500 p-4">{{name}}</p>
+    </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const name = ref('The time in hex is...');
+
+setInterval(() => {
+    const time = new Date().toLocaleTimeString();
+    const timeNumb = time.split(":").join("");
+    const binary = Number(timeNumb).toString(16)
+    name.value = `${time} = ${binary}`;
+}, 1000)
+</script>
